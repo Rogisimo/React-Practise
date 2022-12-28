0bf18027efbbc9ba49b1e6b7110201de63aaed30
kvk-i15-murnikovas.rr <rogisonfleek@gmail.com>
2022-12-28 13:17:53 +0200

    React npm and teranry operator

---
+++ react-practise/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  const name = "Spongebob"
+
+  return (
+    <div className="App">
+      <h1>Hello {name}!</h1>
+      {name ? (<><h1>{name}</h1></>) : (<><h1>test</h1> <h2>There is no name</h2></>)}
+    </div>
+  );
+}
+
+export default App;
